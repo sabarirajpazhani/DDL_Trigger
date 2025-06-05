27222ce90abf85010a2d7e321be67bdb320ea733
Sabari Raj P <143380285+sabarirajpazhani@users.noreply.github.com>
2025-06-05 15:42:57 +0530

    Add files via upload

---
+++ ServerScopeTrigger.sql
@@ -0,0 +1,14 @@
+use DDLTriggers;
+
+/*1. Prevent Login Creation
+Q: How would you prevent unauthorized users from creating new SQL Server logins at the server level?*/
+create trigger trPreventLogin
+on all server
+for create_login
+as
+begin
+	raiserror('Creating new logins is not allowed on this server.',16,1)
+	rollback
+end
+
+create login userLogin with password = 'password@1234';
