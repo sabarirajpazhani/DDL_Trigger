029de04e2392ef09e85e48c63fdbe7470d515b0f
Sabari Raj P <143380285+sabarirajpazhani@users.noreply.github.com>
2025-06-05 16:31:06 +0530

    question 3

--- ServerScopeTrigger.sql
+++ ServerScopeTrigger.sql
@@ -11,4 +11,58 @@ begin
 	rollback
 end
 
+DROP TRIGGER trPreventLogin ON ALL SERVER;
+
 create login userLogin with password = 'password@1234';
+
+/*2. Audit Login Activity
+Q: How can you track who created, altered, or dropped SQL Server logins across the server?*/
+create table master.dbo.AuditLogin(
+	EventType varchar(80),
+	ObjectType varchar(80),
+	EventDate datetime,
+	TriggerBy varchar(80)
+);
+
+drop table AuditLogin;
+
+create trigger trAuditLogin
+on all server
+for create_login, drop_login, alter_login
+as
+begin
+	declare @EventData xml = eventdata()
+	insert into master.dbo.AuditLogin
+	select
+		@EventData.value('(/EVENT_INSTANCE/EventType)[1]','varchar(80)'),
+		@EventData.value('(/EVENT_INSTANCE/ObjectType)[1]','varchar(80)'),
+		getdate(),
+		SYSTEM_USER
+end;
+
+drop trigger trAuditLogin on all server;
+
+CREATE LOGIN useLogin WITH PASSWORD = 'Initial@123';
+
+alter login useLogin with password = 'Password@12345';
+
+select * from AuditLogin;
+
+
+/*3. Block Specific User from Dropping Logins
+Q: How do you stop a specific user like admin1 from dropping server-level logins?*/
+create trigger trBlockSpecificUser
+on all server
+for drop_login
+as
+begin
+	
+	if SYSTEM_USER= 'sa'
+	begin
+		raiserror('sa is not allowed to drop logins.',16,1)
+		rollback
+	end
+end
+
+
+drop login userLogin;
